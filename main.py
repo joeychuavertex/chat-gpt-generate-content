c387cfb796b2c4f84ab7821fac0615cd9da370cb
joeychuavertex <joey.chua@vertexholdings.com>
2023-01-26 16:02:29 +0800

    add text input outside function

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
-st.title("API Key:")
-
 
 def openai_api_key():
     api_key = st.secrets("api_key")
-    st.text_input(api_key)
     return api_key
+
+
+st.title("API Key:")
+st.text_input()
