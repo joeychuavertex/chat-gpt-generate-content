efcb7552f1ddca3ba61f7c00c50a930832ce0c19
joeychuavertex <joey.chua@vertexholdings.com>
2023-01-26 16:40:31 +0800

    change to api key name

--- pages/Translation.py
+++ pages/Translation.py
@@ -1,7 +1,7 @@
 import openai
 import streamlit as st
 
-openai.api_key = st.secrets["openai_api_key"]
+openai.api_key = st.secrets["api_key"]
 
 st.title("Translation")
 response = openai.Completion.create(
