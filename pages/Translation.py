5272b1801a1517587faf710d67ea27030abf3f03
joeychuavertex <joey.chua@vertexholdings.com>
2023-01-26 16:37:03 +0800

    Add translation page

---
+++ pages/Translation.py
@@ -0,0 +1,18 @@
+import openai
+import streamlit as st
+
+openai.api_key = st.secrets("api_key")
+
+st.title("Translation")
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt="Translate this into 1. Japanese 2. Chinese 3. Korean:",
+  temperature=0.3,
+  max_tokens=100,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0
+)
+
+
+st.write(response.choices[0])
