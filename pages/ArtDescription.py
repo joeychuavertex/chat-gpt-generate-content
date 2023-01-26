90509fb66e63dc63a9318fced59971ba9fd3533b
joeychuavertex <joey.chua@vertexholdings.com>
2023-01-26 16:54:23 +0800

    add art description

--- pages/ArtDescription.py
+++ pages/ArtDescription.py
@@ -4,11 +4,11 @@ import streamlit as st
 openai.api_key = st.secrets["api_key"]
 
 st.title("Content")
-input = st.text_input("Generate article content based on:")
+input = st.text_input("Generate description based on:", placeholder="Describe in detail the center of a village during christmas")
 
 response = openai.Completion.create(
   model="text-davinci-003",
-  prompt=f"Generate detailed article based on the topic of {input}",
+  prompt=f"Generate detailed description on {input}",
   temperature=0.3,
   max_tokens=100,
   top_p=1,
