ed27cd3ab9f8844c5aa69e3f42783443fa0bef8c
joeychuavertex <joey.chua@vertexholdings.com>
2023-01-26 17:01:21 +0800

    add social media posts

--- pages/ArtDescription.py
+++ pages/ArtDescription.py
@@ -3,14 +3,14 @@ import streamlit as st
 
 openai.api_key = st.secrets["api_key"]
 
-st.title("Content")
+st.title("Art Description Generator")
 input = st.text_input("Generate description based on:", placeholder="Describe in detail the center of a village during christmas")
 
 response = openai.Completion.create(
   model="text-davinci-003",
   prompt=f"Generate detailed description on {input}",
   temperature=0.3,
-  max_tokens=100,
+  max_tokens=1000,
   top_p=1,
   frequency_penalty=0,
   presence_penalty=0
--- pages/SocialMediaPosts.py
+++ pages/SocialMediaPosts.py
@@ -3,14 +3,14 @@ import streamlit as st
 
 openai.api_key = st.secrets["api_key"]
 
-st.title("Article Content Generator")
-input = st.text_input("Generate article content based on:")
+st.title("Social Media Posts")
+input = st.text_area("Suggest social media posts:", placeholder="Venture Capital")
 
 response = openai.Completion.create(
   model="text-davinci-003",
-  prompt=f"Generate detailed article based on the topic of {input}",
+  prompt=f"Suggest social media posts on {input}",
   temperature=0.3,
-  max_tokens=4000,
+  max_tokens=500,
   top_p=1,
   frequency_penalty=0,
   presence_penalty=0
