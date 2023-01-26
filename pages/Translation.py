ad6e2d017e7379f4df812876ce048ac02ad8cf99
joeychuavertex <joey.chua@vertexholdings.com>
2023-01-26 16:42:55 +0800

    allow user input

--- pages/Translation.py
+++ pages/Translation.py
@@ -4,9 +4,11 @@ import streamlit as st
 openai.api_key = st.secrets["api_key"]
 
 st.title("Translation")
+input = st.text_input("Enter text for translation into Chinese, Japanese, Korean")
+
 response = openai.Completion.create(
   model="text-davinci-003",
-  prompt="Translate this into 1. Japanese 2. Chinese 3. Korean:",
+  prompt=f"Translate {input} into 1. Chinese 2. Japanese 3. Korean:",
   temperature=0.3,
   max_tokens=100,
   top_p=1,
